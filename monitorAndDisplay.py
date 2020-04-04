4cde9289ab2d2783201b8622be7e78578de64529
avishekhbharati <Baba's9804805969>
2020-04-05 01:24:48 +1100

    Problem with json.load is fixed works now in any version of python. Few string formatting and minor changes.

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -2,12 +2,12 @@ from sense_hat import SenseHat
 from time import sleep
 import json
 
-sense = SenseHat()
-RED_COLOR = [255, 0, 0]
-GREEN_COLOR = [0, 255, 0]
-BLUE_COLOR = [0, 0, 255]
 
 class TempreatureMonitor:
+    
+    RED_COLOR = [255, 0, 0]
+    GREEN_COLOR = [0, 255, 0]
+    BLUE_COLOR = [0, 0, 255]
 
     def __init__(self, filename):
         data = self.read_from_file(filename)
@@ -17,28 +17,33 @@ class TempreatureMonitor:
         self.hot_min = data['hot_min']
         self.sense = SenseHat()
     
+    
     def test_values(self):
         print(self.cold_max)
 
+
     def read_from_file(self, filename):
-        with open(filename) as values:
-            return json.load(values)   
-        
+        f = open(filename, 'r')
+        return json.loads(f.read())
+
 
     def read_tempreature(self):
-        return self.sense.get_temperature()
+        return round(self.sense.get_temperature(), 2)
 
     
     def display_current_tempreature(self, temp):
         #check the tempreature and set color
         self.sense.clear()
-
+        
+        str_format = str(temp) + "C"
+        
+        #here max value is inclusive
         if temp <= self.cold_max:
-            self.sense.show_message(str(temp), text_colour = BLUE_COLOR)
+            self.sense.show_message(str_format, text_colour = self.BLUE_COLOR)
         elif temp > self.hot_min:
-            self.sense.show_message(str(temp), text_colour = RED_COLOR)
+            self.sense.show_message(str_format, text_colour = self.RED_COLOR)
         else:
-            self.sense.show_message(str(temp), text_colour = GREEN_COLOR)
+            self.sense.show_message(str_format, text_colour = self.GREEN_COLOR)
 
 
 temp_monitor = TempreatureMonitor('config.json')
@@ -46,5 +51,3 @@ while True:
     curr_temp = temp_monitor.read_tempreature()
     temp_monitor.display_current_tempreature(curr_temp)
     sleep(3)
-
-    
