6d430e5c379ccdf196b94a892257266963d15ccf
Ren Chalissery <renchalissery1994@gmail.com>
2020-04-05 19:35:32 +1000

    updated electronicDie.py

--- electronicDie.py
+++ electronicDie.py
@@ -2,18 +2,17 @@ from sense_hat import SenseHat
 from time import sleep
 from random import randrange
 
-class ElectronicDie:    
-
-    def start(self):
-        sense = SenseHat()
-        while True:
-            x,y,z = [sense.get_accelerometer_raw()[key] for key in ['x','y','z']]
-            if(abs(x) > 1 or abs(y) > 1 or abs(z) > 1):
-                self.display_die(sense, randrange(1, 7))
-                sleep(1)
-                sense.clear()
+class ElectronicDie:
     
-    def display_die(self, sense, number):
+    def roll_die(self):
+        sense = SenseHat()
+        x,y,z = [sense.get_accelerometer_raw()[key] for key in ['x','y','z']]
+        while True:
+            if(abs(x) > 1 or abs(y) > 1 or abs(z) > 1):
+                return self.display_die()
+
+    def display_die(self):
+        number = randrange(1, 7)
         x = (0, 0, 0) # black
         b = (128, 128, 0) # yellow
         dice_array = [[            
@@ -72,6 +71,9 @@ class ElectronicDie:
             b, b, x, b, b, x, b, b
         ]]
         sense.set_pixels(dice_array[number-1])
+        sleep(1.5)
+        sense.clear()
+        return number
 
 electronicDie = ElectronicDie()
-electronicDie.start()
+electronicDie.roll_die()
