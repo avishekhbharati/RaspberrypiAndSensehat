b53e36f93126f86f73c82ac0b51e2d4aa22e346a
avishekhbharati <Baba's9804805969>
2020-04-05 20:17:32 +1000

    Changed the colors brightness.

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -1,13 +1,12 @@
 from sense_hat import SenseHat
 from time import sleep
 
-
 class Emoji:    
 
     def smiley(self):
-        x = (255, 255, 255) #white
-        o = (0, 255, 0) # Green
-        w = (0, 0, 255) # blue
+        x = (225, 225, 225) #white
+        o = (0, 128, 0) # Green
+        w = (0, 0, 128) # blue
         smiley_face = [            
             x, x, x, x, x, x, x, x,
             x, x, x, x, x, x, x, x,
@@ -21,8 +20,8 @@ class Emoji:
         return smiley_face
     
     def frowning(self):
-        x = (255, 0, 0) #red
-        o = (0, 0, 255) # blue
+        x = (128, 0, 0) #red
+        o = (0, 0, 128) # blue
         w = (0, 0, 0) # black
         frowning_face = [            
             x, x, x, x, x, x, x, x,
@@ -37,9 +36,9 @@ class Emoji:
         return frowning_face
 
     def laughing(self):
-        x = (255,255,0) #yellow
-        o = (0, 255, 0) # Green
-        w = (255, 255, 255) # white
+        x = (100,100,0) #yellow
+        o = (200, 100, 200)
+        w = (225, 225, 225) # white
         laughing_face = [            
             x, x, x, x, x, x, x, x,
             x, x, x, x, x, x, x, x,
@@ -50,7 +49,7 @@ class Emoji:
             x, x, w, w, w, w, x, x,
             x, x, x, x, x, x, x, x
         ]
-        return laughing_face
+        return laughing_face        
 
 
 sense = SenseHat()
